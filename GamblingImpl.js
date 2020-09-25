747deb213e27c2d615bef045cb298453958d688e
SoumenGhosh18 <soumentaki1996@gmail.com>
2020-09-25 19:17:10 +0530

    Added : Code to check win or loss till the 50% limit

--- GamblingImpl.js
+++ GamblingImpl.js
@@ -1,19 +1,26 @@
 class GamblingImpl {
     
 bet() {
-   
     var bet =1;
     var Stake =100;
+    while(Stake != 150 && Stake !=50)
+    {
+
     var number = Math.random() *2;
     if(number <=1){
-          Stake--;
+          Stake +=bet;
           console.log("loss");
         }
           else{
-            Stake++;
+            Stake = Stake -bet;
             console.log("Win");
         }
-    }     
+       }
+       if(Stake == 150)
+       console.log("He wins 50% of this stake");
+       else
+       console.log("He lose 50% of his stake");    
+  } 
 }  
 module.exports = new GamblingImpl();
 
